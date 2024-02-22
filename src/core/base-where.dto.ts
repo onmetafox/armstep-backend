1109c2acd5985b112b363fdce5f062f5c69ca449
onmetafox <onemetafox@outlook.com>
2024-02-22 07:44:26 -0800

    feat: designed the backend server structure

---
+++ src/core/base-where.dto.ts
@@ -0,0 +1,3 @@
+export class BaseWhereDto{
+    page: number
+}
---
+++ src/users/users.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BaseService } from 'src/core/base.service';
+import { UsersController } from "./users.controller";
+import { UsersService } from './users.service';
+import { UsersSchema } from 'src/schema/users.model';
+@Module({
+    controllers: [UsersController],
+    imports: [
+        MongooseModule.forFeature([{ name: 'Users', schema: UsersSchema}])
+    ],
+    providers: [
+        // BaseService,
+        UsersService
+    ]
+})
+
+export class UsersModule {}
